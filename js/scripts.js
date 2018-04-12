24ddcf9156415eb5231feda89492158665b536a1
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-11 18:02:38 -0700

    take home for day 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var myHistory = "dank meme lord";
+var myHistory = "my favorite hobby (aside from coding duh) is playing video games and stuff";
 // console.log(myHistory);
 var myValue = confirm(myHistory);
 console.log(myValue);
