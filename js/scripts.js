b3c526f4fac0a348ee441bfb198010187e223c04
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-11 20:59:21 -0700

    day 2 practiced js basics

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const myName = "Ryeker";//declared AND defined
+console.log(myName, "One");
+{
+  const myName = "Stasia"
+  console.log(myName, "Two");
+};
+
+  console.log(myName, "Three");
+
+var test; //declared
+test = 'My test is that I am awesome';//defined
+
+// console.log(test);
+// let birthday = '3/7/95';
+// let rightNow = new Date (birthday);
+// alert(rightNow);
+
+// PROMPT
+const userName = prompt("hahahah?");
+console.log(userName);
