6f2edb8f2c26ce09fb8fb83b706c93e302a60097
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-16 20:58:27 -0700

    day 3 practice code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,45 @@
-// PROMPT TAKE HOME 2
 
-const userName = prompt("Hey you!! I'm a little short on funds and might want to steal your identity if that's cool with you. Let's start with your first name.. ");
-console.log(userName);
 
-alert(userName +"? That's dope, but I'm also gonna need your last name..");
+// Declare a var and set it equal to any string
+// var favoriteBook = "Ready Player One";
+// Declare a second var and call new String() on the same string (this will create and instance of a JS string object.)
+// var aBook = "Ready Player One";
+// Use the logial operators and a fourth var to determine if they are strictly equal.
+// var isEqual = (favoriteBook == aBook);
+// console.log(isEqual);
+// var isEqual = (favoriteBook == aBook);
+// Now use the logical operators and a fourth var to determine if they are loosely equal.
 
-const userLast = prompt("well? I'm waiting... ");
+//
+// var yourName = prompt("What's your name?");
+//
+// console.log(!yourName);
 
-alert(userLast + "? sweet! I think that's all I need.")
+// let obj1 = {
+//   name: "Stasia"
+// };
+//
+// let obj2 = {
+//   name: "Stasia"
+// };
+//
+//
+// console.log(typeof("three" * 15));
+//
+//
+// let myNumber = prompt("GIVE ME A NUMBER!!!")
+//
+// alert(
+//   isNan((parseInt(myNumber) / 2))
+// );
+// let string = prompt("Whats yo favorite number?!?!")
+//
+// string.toString();
+//
+// alert(`Your favorite number is ${string}`);
 
-const userBirth = prompt("WAIT! I forgot I also need your birthday duh. What kind of criminal am I? Don't forget to write it as MM/DD/YYYY")
+let myNumber = prompt("whats yo favorite number?");
 
-userAnswer = new Date(userBirth);
+myNumber = parsInt(myNumber);
 
-alert(userAnswer + "  dang....");
-
-stasiaCoffee = prompt("Will Ryeker bring more coffee next class so stasia can be properly caffeinated? (hint: yes)");
-
-alert("sweet ryeker is the GOAT!!!");
+alert(myNumber + 42, myNumber - 42, myNumber * 42, myNumber / 42);
