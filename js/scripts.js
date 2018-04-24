48e817df239b3b241542cee124e2230ef9e28c4f
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-23 18:05:17 -0700

    updated for day4 hw

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,61 @@
-// PROMPT TAKE HOME 2
+// Day 3 Take Home
 
-const userName = prompt("Hey you!! I'm a little short on funds and might want to steal your identity if that's cool with you. Let's start with your first name.. ");
+
+userName = window.prompt("Hey there. I'm Beyonce, leader of the illuminati. What's your name?");
 console.log(userName);
 
-alert(userName +"? That's dope, but I'm also gonna need your last name..");
+favoriteColor = window.prompt(userName + "? That's a cool name. Well hey, I'm gonna let you in on a little secret. Myself and the Golden Girls are planning world domination. We're going to start off by destroying every color on the planet except one. IF you had to choose one color to remain out of red, orange, yellow, green, blue, pink, purple, black, white, and grey...which would you choose?");
 
-const userLast = prompt("well? I'm waiting... ");
 
-alert(userLast + "? sweet! I think that's all I need.")
+switch (favoriteColor) {
 
-const userBirth = prompt("WAIT! I forgot I also need your birthday duh. What kind of criminal am I? Don't forget to write it as MM/DD/YYYY")
+case "red":
+alert(`Awesome ${userName}! Red is the color of blood, which I have none of.`);
+console.log(favoriteColor);
+break;
 
-userAnswer = new Date(userBirth);
+case "orange":
+alert(`oh dang ${userName}! Orange is a cool color. But not cool enough, so I'm still getting rid of it, sorry.`);
+console.log(favoriteColor);
+break;
 
-alert(userAnswer + "  dang....");
+case "yellow":
+alert(`Nice ${userName}! I also love yellow. It's the color of the sun. I think.`);
+console.log(favoriteColor);
+break;
 
-stasiaCoffee = prompt("Will Ryeker bring more coffee next class so stasia can be properly caffeinated? (hint: yes)");
+case "green":
+alert(`Awesome ${userName}! Green is the color of money, which I have a lot of.`);
+console.log(favoriteColor);
+break;
 
-alert("sweet ryeker is the GOAT!!!");
+case "blue":
+alert(`Blue, huh ${userName}? Hmmm......nice....Illuminati Approved.`);
+console.log(favoriteColor);
+break;
+
+case "pink":
+alert(`I love pink too ${userName}! So do the Golden Girls.`);
+console.log(favoriteColor);
+break;
+
+case "purple":
+alert(`Purple is awesome ${userName}! I guess I'll keep that color.`);
+console.log(favoriteColor);
+break;
+
+case "black":
+alert(`oh no ${userName}! Black?!?! Cool. Sounds emo.`);
+console.log(favoriteColor);
+break;
+
+case "white":
+alert(`White is cool ${userName}! RADICAL.`);
+console.log(favoriteColor);
+break;
+
+case "gray":
+alert(`Nice ${userName}! Gray can stay.`);
+console.log(favoriteColor);
+break;
+}
