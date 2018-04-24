1ed787298869cb89dd57cf06e31faedb2d27c676
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-23 17:15:04 -0700

    js practice day4

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,63 @@
+// let cars = ["jeep", "subaru", "honda"];
+// // console.log(cars[1]);
+// // console.log(cars[1].length);
+// cars.push("bmw");
+// console.log(cars);
+//
+// var removedCar = cars.pop()
+// console.log(cars);
+
+let arr = ["Christa", "Abel", "Gideon", "Zeta", "Bobby"];
+
+let backwards = arr.sort();
+console.log(backwards);
+
+// let bestEver = arr.slice();
+//
+// let noCoteacher = arr.slice(0);
+//
+// let teacherIndex = arr.indexOf("ryeker");
+//
+// let onlyStudents = arr.splice(teacherIndex, 1);
+//
+// console.log(teacherIndex);
+
+// bestEver.push('Christa');
+
+// arr.shift();
+// console.log(arr);
 
 
-// Declare a var and set it equal to any string
-// var favoriteBook = "Ready Player One";
-// Declare a second var and call new String() on the same string (this will create and instance of a JS string object.)
-// var aBook = "Ready Player One";
-// Use the logial operators and a fourth var to determine if they are strictly equal.
-// var isEqual = (favoriteBook == aBook);
-// console.log(isEqual);
-// var isEqual = (favoriteBook == aBook);
-// Now use the logical operators and a fourth var to determine if they are loosely equal.
 
-//
-// var yourName = prompt("What's your name?");
-//
-// console.log(!yourName);
-
-// let obj1 = {
-//   name: "Stasia"
-// };
-//
-// let obj2 = {
-//   name: "Stasia"
-// };
-//
-//
-// console.log(typeof("three" * 15));
-//
-//
-// let myNumber = prompt("GIVE ME A NUMBER!!!")
-//
-// alert(
-//   isNan((parseInt(myNumber) / 2))
-// );
-let string = prompt("Whats yo favorite number?!?!")
-
-string.toString();
-
-alert(`Your favorite number is ${string}`);
-
-// let myNumber = prompt("whats yo favorite number?");
-// 
-// myNumber = parsInt(myNumber);
-//
-// alert(myNumber + 42, myNumber - 42, myNumber * 42, myNumber / 42);
+// function chooseAdventure() {
+//     while (true) {
+//         var location = prompt("Would you like to go to the store or relax at home? Respond by typing store or home", 'store, home');
+//         if (location == "store") {
+//             var what = prompt("Are you going to bake or bbq?", 'bake, bbq');
+//         }
+//         if (what == "bake") {
+//             prompt("You made some awesome cupcakes!");
+//             break;
+//         }
+//         if(what == "bbq") {
+//             prompt("You made some awesome kabobs!");
+//             break;
+//         }
+//         if (location == "home") {
+//             var activity = prompt("Would you like to go to watch netflix or go swimming?", 'netflix, swimming');
+//         }
+//         if (activity == "netflix") {
+//             alerts("You watched Jessica Jones");
+//             break;
+//         }
+//         if (activity == "swimming") {
+//             prompt("You cooled off in the pool");
+//             break;
+//         }
+//         else {
+//             prompt("Sorry, you entered the wrong input or it wasn't lower-case.");
+//             break;
+//         }
+//     }
+// }
+// chooseAdventure();
