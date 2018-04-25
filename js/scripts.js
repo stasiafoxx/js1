da3acb42b63ad7c31cb3a0ebd92ac00aa0b2ffc6
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-24 22:42:19 -0700

    first attempt at day4

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,8 @@
-// Day 3 Take Home
+// Day 4 Take Home
 
+let userTodo =[''];
 
-userName = window.prompt("Hey there. I'm Beyonce, leader of the illuminati. What's your name?");
-console.log(userName);
+userTodo = window.prompt("What are three things you need to get done this weekend? \n\n For example: shopping, homework, laundry, take a shower..try and seperate them with commas!")
+console.log(userTodo)
 
-favoriteColor = window.prompt(userName + "? That's a cool name. Well hey, I'm gonna let you in on a little secret. Myself and the Golden Girls are planning world domination. We're going to start off by destroying every color on the planet except one. IF you had to choose one color to remain out of red, orange, yellow, green, blue, pink, purple, black, white, and grey...which would you choose?");
-
-
-switch (favoriteColor) {
-
-case "red":
-alert(`Awesome ${userName}! Red is the color of blood, which I have none of.`);
-console.log(favoriteColor);
-break;
-
-case "orange":
-alert(`oh dang ${userName}! Orange is a cool color. But not cool enough, so I'm still getting rid of it, sorry.`);
-console.log(favoriteColor);
-break;
-
-case "yellow":
-alert(`Nice ${userName}! I also love yellow. It's the color of the sun. I think.`);
-console.log(favoriteColor);
-break;
-
-case "green":
-alert(`Awesome ${userName}! Green is the color of money, which I have a lot of.`);
-console.log(favoriteColor);
-break;
-
-case "blue":
-alert(`Blue, huh ${userName}? Hmmm......nice....Illuminati Approved.`);
-console.log(favoriteColor);
-break;
-
-case "pink":
-alert(`I love pink too ${userName}! So do the Golden Girls.`);
-console.log(favoriteColor);
-break;
-
-case "purple":
-alert(`Purple is awesome ${userName}! I guess I'll keep that color.`);
-console.log(favoriteColor);
-break;
-
-case "black":
-alert(`oh no ${userName}! Black?!?! Cool. Sounds emo.`);
-console.log(favoriteColor);
-break;
-
-case "white":
-alert(`White is cool ${userName}! RADICAL.`);
-console.log(favoriteColor);
-break;
-
-case "gray":
-alert(`Nice ${userName}! Gray can stay.`);
-console.log(favoriteColor);
-break;
-}
+userTodo.push 
