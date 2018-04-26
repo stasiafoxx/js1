d1d70339d48db66c7d059f45e30105b7894a8728
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-25 20:50:01 -0700

    practiced day5 takehome #2

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,22 @@
 //   let myGreeting = sayHello("Connie", "You're the greatest!");
 //
 //   alert(myGreeting);
-let dogsAge = parseInt(prompt("Let's find out how old your furry friend is in dog years! \n\n What is your dog friend's current age (in human years)?", "3"));
+// let dogsAge = parseInt(prompt("Let's find out how old your furry friend is in dog years! \n\n What is your dog friend's current age (in human years)?", "3"));
+//
+// function dogAgeCalculator(age) {
+//   return age * 7;
+// }
+//
+// if(!isNaN(dogsAge)) {
+//   alert(dogAgeCalculator(dogsAge));
+// }
+//
+//
+// alert(`Your pupper is ${dogAge} years old!! Heckin CUTE!`);
 
-function dogAgeCalculator(age) {
-  return age * 7;
+let cheeseAge = parseInt(prompt("Let's figure out much cheese you would need for your WHOLE LIFE! \n\n First of all, how old are you?"));
+
+function(cheeseAge, amountPerDay) {
+  const yearsLeft = 80 - cheeseAge
+  yearsLeft * 365
 }
-
-if(!isNaN(dogsAge)) {
-  alert(dogAgeCalculator(dogsAge));
-}
-
-
-alert(`Your pupper is ${dogAge} years old!! Heckin CUTE!`);
