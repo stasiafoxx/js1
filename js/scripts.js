54d9e73a70ebfec00335b279dbbfbef987fdcd82
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-04-30 20:55:54 -0700

    day 6/7 practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-let blueDivs = document.getElementsByClassName('blue');
+// let blueDivs = document.getElementsByClassName('blue');
+// let h1 = document.getElementsByTagName('h1');
+// let para = document.getElementById('para');
+// let queryBlues = document.querySelector('h1');
+// console.log(queryBlues.clientHeight);
+
+// function queryDom(element, method) {
+//   let elementToQuery = document.method('element');
+//   console.log(elementToQuery);
+// }
+//
+// let el = prompt("give me an element");
+// let method = prompt("Give me a method");
+//
+const button =
+document.querySelector('btn-danger');
+
+button.addEventListener('click', (e)
+=> {
+  alert('ahhh dratz) ${e.target}');
+});
+
+div.addEventListener('mouseenter'),
+(e) => {
+  e.target.backgroundColor = 'red';
+}
